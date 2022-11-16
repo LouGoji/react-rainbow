116abf2114095ca6cb02e11181e33d9b45c4e869
Louis Mantell <loiuismantell@Louis-iMac.local>
2022-11-16 17:12:45 -0500

    rainbow

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ import './App.css';
 
 function App() {
   let [colors, setColors] = useState(['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red'])
- 
-  const addColor = (newColor) => {
-    setColors([...colors, newColor])
+  let colorMap = colors.map((color, i) => {
+    return (
+      <ColorBlock key={i} color={color} />
+    )
+  })
+
+   const addColor = (newColor) =>{
+    setColors([...colors,newColor])
   }
-  
   return (
     <div className="App">
-    { colors.map((color, index) =>
-    <ColorBlock color={color} key={color} />
-    )}
+      {colorMap}
+      <ColorForm addColor={addColor}/>
     </div>
   );
 }
 
 export default App;
-
