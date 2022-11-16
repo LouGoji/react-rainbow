48bf3187d7ab51034d5c69a71b6e288bd640ba90
Louis Mantell <loiuismantell@Louis-iMac.local>
2022-11-16 17:03:12 -0500

    rainbow

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
+import ColorBlock from './colorBlock'
+
 
 function App() {
+  let [colors, setColors] = useState(['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red'])
+ 
+  const addColor = (newColor) => {
+    setColors([...colors, newColor])
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    { colors.map((color, index) =>
+    <ColorBlock color={color} key={color} />
+    )}
     </div>
   );
 }
 
 export default App;
+
